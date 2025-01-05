0444213e8b9332e11f40bad5f6e799fcee6f2aa8
Calvince Owino <www.owinocalvince@gmail.com>
2025-01-05 08:12:10 +0300

    made some changes in the models

--- src/models/contactus_model.php
+++ src/models/contactus_model.php
@@ -5,7 +5,7 @@
  * @see       https://github.com/jalikoa/fgi/ The future guardians repository
  * @author    Calvince Owino Jalikoa (Kenya) <d34calvo@gmail.com>
  * @copyright 2024 - 2025 Calvince Owino CEO Jalsoft
- * @license   Thi programe is written for the specific needs of future guardians initiative and no part can be taken away without prior permissions
+ * @license   This programe is written for the specific needs of future guardians initiative and no part can be taken away without prior permissions
  * CEO JalSoft Calvince Owino
  * @see contact at +254799311413
  */
