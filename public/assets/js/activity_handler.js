a414ca9b52c54826a305ed8aa1fdff27de2efa77
Calvince Owino <www.owinocalvince@gmail.com>
2025-01-13 00:35:54 +0300

    made some small update on the activity handler js files

--- public/assets/js/activity_handler.js
+++ public/assets/js/activity_handler.js
@@ -1,3 +1,5 @@
+var activityName,activityDescription,activityFile,activityForm;
+
 function deleteActivity(){
     Swal.fire({
         title: 'Please confirm!',
@@ -13,4 +15,38 @@ function deleteActivity(){
             Swal.fire('Deleted','Delete successful','success');
         }
     });
+}
+activityForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    AddActivity();
+})
+function AddActivity(){
+    const addXhr = checkXml();
+    addXhr.open('POST',route,true);
+    setHeader(addXhr);
+    addXhr.onload = ()=>{
+
+    }
+    addXhr.onerror = ()=>{
+        
+    }
+    const data = new FormData();
+    // append the necesary here 
+    addXhr.send(data);
+}
+function fetchActivities(){
+
+}
+function searchActivity(){
+
+}
+function fetchRecentActivity(){
+
+}
+function isShort(len,str){
+    return(str.length >= len)?true:false;
+}
+function isText(text){
+    let textRegExp = /^[A-Za-zÀ-ÿ\s]+(?:['’][A-Za-zÀ-ÿ]+)*$/;
+    return (textRegExp.test(text))?true:false;
 }
